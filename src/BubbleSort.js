4c42da3fda783622ed6edded39bd415a5ce7ab82
Tri Do <trido1903@gmail.com>
2020-02-24 22:12:24 -0800

    changed color of the bar

--- src/BubbleSort.js
+++ src/BubbleSort.js
@@ -1,18 +1,20 @@
 import React, { Component } from "react";
 function BubbleSort(array) {
-  // const{arrays} = this.state;
-  console.log(array);
-  for (let i = array.length; i > 0; i--) {
-    for (let j = 0; j < i; j++) {
-      if (array[j] > array[j + 1]) {
-        let temp = array[j];
-        setTimeout(() => {
-            array[j] = array[j + 1];
-            array[j + 1] = temp;
-        }, i * 3000);
-      }
-    }
-  }
+
+  // for (let i = array.length; i > 0; i--) {
+  //   for (let j = 0; j < i; j++) {
+  //     if (array[j] > array[j + 1]) {
+  //         console.log("...");
+  //       setTimeout(() => {
+  //           let temp = array[j];
+  //           array[j] = array[j + 1];
+  //           array[j + 1] = temp;
+  //           newArr.push(array[j]);
+  //           newArr.push(array[j + 1]);
+  //       }, i * 1000);
+  //     }
+  //   }
+  // }
   console.log(array);
 }
 
--- src/Rectangle.js
+++ src/Rectangle.js
@@ -2,34 +2,65 @@ import React, { Component } from "react";
 import { BubbleSort } from "./BubbleSort";
 import "./Rectangle.css";
 
+const PRIMARY_COLOR = "aquamarine";
+const PROCESSING_COLOR = "red";
+const SPEED_MS = 1;
+
 class Rectangle extends Component {
   constructor() {
     super();
     this.state = {
-      arrays: []
+      array: []
     };
     this.generateArray = this.generateArray.bind(this);
     this.bubbleSort = this.bubbleSort.bind(this);
   }
 
   generateArray() {
-    const size = 20;
+    const size = 30;
     const newArr = [];
     for (let i = 0; i < size; i++) {
       const rand = Math.floor(Math.random() * 300) + 1;
       newArr.push(rand);
     }
-    this.setState({ arrays: newArr });
+    this.setState({ array: newArr });
   }
 
-  bubbleSort(){
-    BubbleSort(this.state.arrays);
+  bubbleSort() {
+    const { array } = this.state;
+
+    for (let i = array.length; i > 0; i--) {
+      for (let j = 0; j < i - 1; j++) {
+        const barEl = document.getElementsByClassName("single-rect");
+        const bar1Style = barEl[j].style;
+        const bar2Style = barEl[j + 1].style;
+        setTimeout(() => {
+          bar1Style.backgroundColor = PROCESSING_COLOR;
+          bar2Style.backgroundColor = PROCESSING_COLOR;
+          if (array[j] > array[j + 1]) {
+            let temp = array[j];
+            array[j] = array[j + 1];
+            array[j + 1] = temp;
+            let height = bar1Style.height;
+            bar1Style.height = bar2Style.height;
+            bar2Style.height = height;
+          }
+        }, SPEED_MS * 2000);
+        bar1Style.backgroundColor = PRIMARY_COLOR;
+        bar2Style.backgroundColor = PRIMARY_COLOR;
+      }
+    }
+    this.setState({array: array});
   }
 
   render() {
-    const { arrays } = this.state;
-    const rectEle = arrays.map((value, idx) => (
-      <div className="single-rect" style={{ height: `${value}px` }} key={idx}>
+    const { array } = this.state;
+    const rectEle = array.map((value, idx) => (
+      <div
+        className="single-rect"
+        style={{ height: `${value}px`, backgroundColor: `${PRIMARY_COLOR}` }}
+        key={idx}
+      >
         {value}
       </div>
     ));
